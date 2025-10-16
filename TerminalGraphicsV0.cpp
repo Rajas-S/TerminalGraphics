ae2a9cc357972b73bd26df8d63c0d8a08bab6d3c
Rajas-S <dr.rajas.sharma@gmail.com>
2025-10-16 07:19:23 +0100

    change grid from static array to 2d vector
    change grid from static array to 2d vector - this means the data for grid is stored on the heap and not on the limited stack which was throwing warnings earlier - now there is no need to predefine a MAXGRIDSIDE const which was very expensive and wasteful and ineffective for general use

--- TerminalGraphicsV0.cpp
+++ TerminalGraphicsV0.cpp
@@ -5,7 +5,7 @@
 
 #include <iostream>
 #include <thread>
-#define MAXGRIDSIDE 400//the maximum size of grid array square rooted - the max side lengths of the rect grid window
+#include <vector>
 #define PI 3.14159265358979323846
 struct TerminalGraphics {
 private:
@@ -21,14 +21,30 @@ public:
 	int wallx, wally;
 	int framerate;
 	int tiledepth;
-	int grid[MAXGRIDSIDE][MAXGRIDSIDE] = {0};
+	std::vector<std::vector<int>> grid;
 	std::string tileset;
+	std::string border= "  ";
 	std::string world;//the string that is printed to the terminal every frame
+	bool gamestate=true;
 
 	// ------- initialisation functions -------
-	void setWindowSize(const int wallx, const int wally) {this->wallx = wallx; this->wally = wally;}//set wallx and wally
+	void setWindow(int wallx, int wally) {
+		this->wallx = wallx; this->wally = wally;//set wallx and wally
+		std::vector<int> temp;
+		for (int i = 0; i < wallx; i++) {
+			temp.clear();
+			for (int j = 0; j < wally; j++) {
+				temp.push_back(0);
+			}
+			grid.push_back(temp);
+		}
+	}
 	void setFramerate(int framerate) {this->framerate = framerate;}
 	void setTileset(std::string tileset) { this->tileset = tileset; tiledepth = tileset.length(); }//create a tileset using chars - "123" --> "1" is tile 0
+	void setBorder() {
+		std::string border = "|-";
+	}
+
 
 	// ------- world functions -------
 	void sleep() { std::this_thread::sleep_for(std::chrono::microseconds(1000000 / framerate)); }
@@ -46,8 +62,10 @@ public:
 				world += tileset[grid[j][i]];
 				world += tileset[grid[j][i]];
 			}
+			world += border[0];
 			world += '\n';
 		}
+		for (int i = 0; i < wallx; i++) {world += border[1];}
 		std::cout << "\033[2J\033[1;1H";
 		std::cout << world;
 	}
@@ -56,6 +74,7 @@ public:
 
 	//takes many small steps from point one to point two and 
 	//sets the grid position that is lands on to specified int
+	//used for drawing lines
 	void line(int x1, int y1, int x2, int y2, int steps, int value) {
 		int vx =x2-x1; int vy = y2-y1;
 		double error = 0.01;
@@ -70,7 +89,10 @@ public:
 		}
 	}
 
-	//an attempt at using Bresenham's line algorithm :) --> find out more at https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+	//an attempt at using Bresenham's line algorithm :) 
+	// --> find out more at https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+	//more efficient than line() function but produces strange results when paired with polygon function below
+	//especially in the triangle where some lines dont appear
 	void lineB(double x1, double y1, double x2, double y2, int value) {
 		int x, y;
 		int dir=1;
@@ -110,14 +132,35 @@ public:
 			theta += (PI*2) / (vertecies);
 			double deltaX1 = radius * cos(theta);
 			double deltaY1 = radius * sin(theta);
-			line((int)(x + deltaX), (int)(y + deltaY), (int)(x + deltaX1), (int)(y + deltaY1),1000,value);
+			line((int)(x + deltaX), (int)(y + deltaY), (int)(x + deltaX1), (int)(y + deltaY1),resolution,value);
+			deltaX = deltaX1;
+			deltaY = deltaY1;
+		}
+	}
+	void regPolygon(double x, double y, double radius, int vertecies, int value,double theta1) {
+		double error = 0.01;
+		double theta = theta1;
+		int resolution = 2000;
+		double deltaX = radius * cos(theta);
+		double deltaY = radius * sin(theta);
+		for (int i = 0; i < vertecies; i++) {
+			theta += (PI * 2) / (vertecies);
+			double deltaX1 = radius * cos(theta);
+			double deltaY1 = radius * sin(theta);
+			line((int)(x + deltaX), (int)(y + deltaY), (int)(x + deltaX1), (int)(y + deltaY1), resolution, value);
 			deltaX = deltaX1;
 			deltaY = deltaY1;
 		}
 	}
 
 	//unfinished
-	void rect(){}
+	void rect(double x1,double y1, double x2, double y2, int value){
+		int steps = 1000;
+		line(x1, y1, x2, y1,steps,value);
+		line(x2, y1, x2, y2, steps, value);
+		line(x2, y2, x1, y2, steps, value);
+		line(x1, y2, x1, y1, steps, value);
+	}
 };
 
 
@@ -129,31 +172,38 @@ public:
 #include <ctime>
 int main() {
 	TerminalGraphics tgl;
+	tgl.setWindow(800, 450);
+	tgl.setTileset("`SNB");
+	tgl.setFramerate(12);
+	tgl.setBorder();
+	int a = 1;
 	srand(std::time(0));
 	//int x = (tgl.wallx * rand() / 32767), y = (tgl.wally*rand()/32767);
-	int x = 0, y = 0;
-	int x1 = 299, y1 = 0;
-	double vx1 = 4.5665, vy1 = 2.6554;
-	double vx =20.343, vy = 7.445;
+	double friction = -0.67;
+	int x = rand()%tgl.wallx-1, y = rand()%tgl.wally-1;
+	int x1 = rand() % tgl.wallx-1, y1 = rand()%tgl.wally-1;
+	double vx1 = (rand()%10000)/10 + 1, vy1 = (rand() % 10000)/10 + 1;
+	double vx = (rand() % 10000) / 10 + 1, vy = (rand() % 10000) / 10 + 1;
 	int scalar = 1;
-	tgl.setWindowSize(300, 300);
-	tgl.setTileset("`_@");
-	tgl.setFramerate(24000);
 	/*tgl.lineB(100, 200, 100, 100, 2);
 	tgl.render();*/
-	while (true) {
+	while (tgl.gamestate) {;
 		tgl.clear(0);
-		if (x >= tgl.wallx) { x = tgl.wallx; vx *= -1; }
-		if (y >= tgl.wally) { y = tgl.wally - 1; vy *= -1; }
-		if (x <= 0) { x = 0; vx *= -1; }
-		if (y <= 0) { y = 0; vy *= -1; }
-		if (x1 >= tgl.wallx) { x1 = tgl.wallx; vx1 *= -1; }
-		if (y1 >= tgl.wally) { y1 = tgl.wally - 1; vy1 *= -1; }
-		if (x1 <= 0) { x1 = 0; vx1 *= -1; }
-		if (y1 <= 0) { y1 = 0; vy1 *= -1; }
+		//return 1;
+		if (x >= tgl.wallx) { x = tgl.wallx- 1; vx *= friction; vy *= -friction; }
+		if (y >= tgl.wally) { y = tgl.wally - 1; vy *= friction; vx *= -friction;}
+		if (x <= 0) { x = 0; vx *= friction; vy *= -friction;}
+		if (y <= 0) { y = 0; vy *= friction; vx *= -friction;}
+		if (x1 >= tgl.wallx) { x1 = tgl.wallx -1; vx1 *= friction; vy1 *= -friction;}
+		if (y1 >= tgl.wally) { y1 = tgl.wally - 1; vy1 *= friction; vx1 *= -friction;}
+		if (x1 <= 0) { x1 = 0; vx1 *= friction; vy1 *= -friction;}
+		if (y1 <= 0) { y1 = 0; vy1 *= friction; vx1 *= -friction;}
 		tgl.grid[x][y] = 1;
-		tgl.regPolygon(x, y, 10, 6, 2);
-		tgl.lineB(x1, y1, x1 - 10*vx1, y1 - 10*vy1,1);
+		//tgl.rect(x-10, y-10, x+10, y+10, 2);
+		tgl.regPolygon(225,225,100,12,3);
+		tgl.line(x, y, x + 2 * vx, y + 2 * vy,100,2);
+		tgl.regPolygon(x1, y1, 10, 12, 1,PI/2);
+		tgl.line(x1, y1, x1 + 2*vx1, y1 + 2*vy1,100,1);
 		tgl.grid[x1][y1] = 1;
 		tgl.render();
 		tgl.sleep();
