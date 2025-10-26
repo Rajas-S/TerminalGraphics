0b5f7260f22350e7bb7fe10488219440b481b6cf
Rajas-S <dr.rajas.sharma@gmail.com>
2025-10-26 07:43:43 +0000

    fixed some stuff - round to nearest int rather than truncate - added maxBrightness to play with...

--- TerminalGraphicsV1.1/ImageEncoding/ImageEncoder.py
+++ TerminalGraphicsV1.1/ImageEncoding/ImageEncoder.py
@@ -1,6 +1,3 @@
-
-#a simple image encoder for images - CONVERTS IMAGES TO GREYSCALE
-
 from PIL import Image
 
 def encodeImage(name, tilesetSize):
@@ -9,12 +6,26 @@ def encodeImage(name, tilesetSize):
     width, height = img.size;
     print(f"width:{width}, height:{height}")
 
+    maxBrightness = 0
+
+    for y in range(height):
+        for x in range(width):
+            if(img.getpixel((x,y))>maxBrightness):
+                maxBrightness=img.getpixel((x,y))
+
+
+    #----------TEMP---------
+    #maxBrightness=0
+    #-----------------------
+
     with open("output.txt",'w') as f:
         for y in range(height):
             for x in range(width):
                 brightness = img.getpixel((x,y))
-                roundedBrightness = int((tilesetSize-1)*brightness/255) #set to an int between 0 and tilesetSize-1 to become reference to index in tilseset
+                roundedBrightness = round((tilesetSize-1)*brightness/(255)) #set to an int between 0 and tilesetSize-1 to become reference to index in tilseset
                 f.write(f"{roundedBrightness:03d}"+" ") #write to output.txt
             f.write("\n")
 
     print("done encoding...")
+
+encodeImage('imgs/CGOLtestimage.jpg',2)
